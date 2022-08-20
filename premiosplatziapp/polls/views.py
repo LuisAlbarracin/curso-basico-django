2e3f1a67a856b192e7a7ce858341d4c38ffc3298
Luis Vicente Albarracin <luisalbarracin9727@gmail.com>
2022-08-20 14:02:18 -0500

    views

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -4,4 +4,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World')
+    return HttpResponse('Estas en la página principal de Premios Platzi App')
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estás viendo la pregunta número {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estás viendo los resultados de la pregunta número {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estás votando a la pregunta número {question_id}")
